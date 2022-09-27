8e9ff3556dd96ddf7d98dbad39a44470366472ef
n-takeshita <naokisyuu@gmail.com>
2022-09-28 01:40:02 +0900

    refactor: use-local-storage

--- modules/use-local-storage.ts
+++ modules/use-local-storage.ts
@@ -1,23 +1,18 @@
-import { Dispatch, SetStateAction, useEffect, useState } from "react"
+import { Dispatch, SetStateAction, useState } from "react"
 
 export const useLocalStorage = <S>(key: string, defaultValue: S): [S, Dispatch<SetStateAction<S>>] => {
-    // window object does not exist during SSR
-    const [value, setValue] = useState<S>(typeof window !== 'undefined' ? getInitValue() : defaultValue);
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    useEffect(() => setValue(getInitValue()), []);
-
-    useEffect(() => {
-        window.localStorage.setItem(key, JSON.stringify(value));
-    }, [value, setValue, key]);
-
-    function getInitValue() {
+    const [value, setValue] = useState<S>(() => {
         const jsonValue = window.localStorage.getItem(key);
         if (jsonValue === 'undefined') return undefined;
         if (jsonValue !== null) return JSON.parse(jsonValue);
 
         return defaultValue;
+    });
+
+    const __setValue = (value: SetStateAction<S>) => {
+        setValue(value)
+        window.localStorage.setItem(key, JSON.stringify(value));
     }
 
-    return [value, setValue];
+    return [value, __setValue];
 }
